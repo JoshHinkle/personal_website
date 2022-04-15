325e5c9c473a173d41929d1fa09e00162704e6b3
Josh Hinkle <joshhinkle@Joshs-Air.lan>
2022-04-15 11:45:51 -0700

    Cleaned up dates, added the blog post

--- _js/javascript.js
+++ _js/javascript.js
@@ -1,13 +1,4 @@
-
-/*
-setTimeout("document.bgColor='white'", 1000) ;
-setTimeout("document.bgColor='lightpink'", 1500); 
-setTimeout("document.bgColor = 'pink'", 2000) ;
-setTimeout("document.bgColor = 'deeppink'", 2500); 
-setTimeout("document.bgColor = 'red'", 3000) ;
-setTimeout("document.bgColor = 'tomato'", 3500); 
-setTimeout("document.bgColor = 'darkred'", 4000);
-*/
+// Yes, I did this in vanilla JS. NO REGRETS 
 
 var e = document.getElementById("first-intro-typing-div");
 e.addEventListener("animationend", function(){
@@ -20,28 +11,17 @@ f.addEventListener("animationend", function(){
   setTimeout(finishSecondAnimation,2500);
 }, false);
 
-
-
-
-
 function finishSecondAnimation(){
   f.classList.remove('typewriter');
   unfade(document.getElementById('arrowVertDiv'))
- 
 }
 
 function addSecondAnimation(){
   e.classList.remove('typewriter');
- 
   f.className = "typewriter";
-  
-  //secondDiv.style.animation = "typing 3.5s steps(29, end), blink-caret .5s step-end infinite";
   var secondText = document.getElementById("second-intro-typing-txt");
-
   secondText.style.textIndent = '0px'
   secondText.innerHTML = "My name is Josh.";
-
-
 }
 
 function unfade(element) {
